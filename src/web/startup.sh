2935ff77b8382dc5096f3d99185926057191356c
Chris' Mac <chris@MacBook.local>
2016-11-14 00:49:25 -0700

    Updated with DC stack variable

--- src/web/startup.sh
+++ src/web/startup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-echo "Changing DEVICE_API_URL to $DEVICE_API_URL..."
-echo "var DEVICE_API_URL = '$DEVICE_API_URL';" > /usr/share/nginx/html/config.js
+if [ -z "$DEVICE_API_URL_OVERRIDE"]; then
+    echo "Changing DEVICE_API_URL to $DEVICE_API_URL_OVERRIDE..."
+    echo "var DEVICE_API_URL = '$DEVICE_API_URL_OVERRIDE';" > /usr/share/nginx/html/config.js
+else
+    echo "Changing DEVICE_API_URL to $DEVICE_API_URL..."
+    echo "var DEVICE_API_URL = '$DEVICE_API_URL';" > /usr/share/nginx/html/config.js
+fi
+
 
 echo "Running: nginx -g daemon off;" 
 nginx -g "daemon off;"
