6e35dcc1a9deb1bd2d6ae173bb004aa04c9087ea
Chris' Mac <chris@MacBook.local>
2016-11-14 00:59:46 -0700

    Inverted if in startup script

--- src/web/startup.sh
+++ src/web/startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [ -z "$DEVICE_API_URL_OVERRIDE"]; then
-    echo "Changing DEVICE_API_URL to $DEVICE_API_URL_OVERRIDE..."
-    echo "var DEVICE_API_URL = '$DEVICE_API_URL_OVERRIDE';" > /usr/share/nginx/html/config.js
-else
+if [ -z "$DEVICE_API_URL_OVERRIDE" ]; then
     echo "Changing DEVICE_API_URL to $DEVICE_API_URL..."
     echo "var DEVICE_API_URL = '$DEVICE_API_URL';" > /usr/share/nginx/html/config.js
+else
+    echo "Changing DEVICE_API_URL to $DEVICE_API_URL_OVERRIDE..."
+    echo "var DEVICE_API_URL = '$DEVICE_API_URL_OVERRIDE';" > /usr/share/nginx/html/config.js
 fi
 
 
